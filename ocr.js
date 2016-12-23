c7f0b1c9aa4923f2d3a44786222df90f5c37621e
Devendra <devendra.rathore@47billion.com>
2016-12-23 12:23:48 +0530

    updated path for log file and logging all uncaught errors

--- ocr.js
+++ ocr.js
@@ -1,4 +1,11 @@
 require('rootpath')();
 var log = require('app/utils/logger')(module);
 log.info('=>starting server');
+
+// uncaught exception
+process.on('uncaughtException', function (err) {
+    log.error('uncaughtException:', err.message);
+    log.error(err.stack);
+});
+
 require('./app')
